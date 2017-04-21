252c3a110cdfa8edfc4be67cdf60a248b85eb30d
kalgcny09 <kalgcny@gmail.com>
2017-04-21 13:07:20 -0400

    updates to game

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import pygame
 
+from math import fabs
+
+from random import randint
+
+from pygame.sprite import spritecollide
+
 pygame.init()
 
 screen = {
@@ -25,16 +31,38 @@ keys_down = {
 hero = {
 	"x": 100,
 	"y": 100,
-	"speed": 10
+	"speed": 10,
+	"wins": 0
+}
+
+monster = {
+	"x": 300,
+	"y": 300,
+	"speed": 20
+}
+
+power_up = {
+	"x": 250,
+	"y": 250
+
 }
 
 
 screen_size = (screen["height"], screen["width"])
 pygame_screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Goblin Chase")
-background_image = pygame.image.load('./images/background.png')
+
+background_image = pygame.image.load('./images/battle1.png')
+
 hero_image = pygame.image.load('./images/hero.png')
 
+monster_image = pygame.image.load('./images/LV.png')
+monster_image_scaled = pygame.transform.scale(monster_image, (50, 50))
+
+power_up_image = pygame.image.load('./images/butterbeer.png')
+
+pygame.mixer.music.load('./sounds/music.wav')
+pygame.mixer.music.play(-1)
 
 
 ####Main Game Loop Begins here
@@ -68,6 +96,7 @@ while game_on:
 			if event.key == keys ['right']:
 				keys_down['right'] = False
 
+
 	if keys_down['up']:
 		hero['y'] -= hero['speed']
 	elif keys_down['down']:
@@ -80,6 +109,39 @@ while game_on:
 	### Render (these items layer so background should be first)
 	pygame_screen.blit(background_image, [0,0])
 
+	font = pygame.font.Font(None, 25)
+	wins_text = font.render("wins: %d" % (hero['wins']), True, (0,0,0))
+	pygame_screen.blit(wins_text, [40,40])
+
 	pygame_screen.blit(hero_image, [hero['x'], hero['y']])
 
+	pygame_screen.blit(monster_image, [monster['x'], monster['y']])
+
+	pygame_screen.blit(power_up_image, [power_up['x'], power_up['y']])
+
 	pygame.display.flip()
+
+
+	## collision detection
+	distance_between = fabs(hero['x'] - monster['x']) + fabs(hero['y'] - monster['y'])
+	if (distance_between < 32):
+		rand_X = randint(0, screen['width'])
+		rand_Y = randint(0, screen['height'])
+		monster['x'] = rand_X
+		monster['y'] = rand_Y
+
+		hero['wins'] += 1
+
+
+
+	## Coin Power up
+	distance_between = fabs(hero['x'] - power_up['x']) + fabs(hero['y'] - power_up['y'])
+	if (distance_between < 32):
+		rand_X = randint(0, screen['width'])
+		rand_Y = randint(0, screen['height'])
+		power_up['x'] = rand_X
+		power_up['y'] = rand_Y
+
+		hero['speed'] -= 5
+
+
